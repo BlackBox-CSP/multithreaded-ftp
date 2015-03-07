ffa0d155f99608502aa924e52857b672da20de17
ryman1 <ryman1@gmail.com>
2015-03-06 20:03:14 -0500

    commit latest version of code

--- multithreaded-ftp.py
+++ multithreaded-ftp.py
@@ -1,33 +1,51 @@
 #!/usr/bin/python
-
+import time
 import threading
 import Queue
-import time
+import ftplib
 
-class WorkerThread(threading.Thread) :
-	def __init__(self, queue, id):
+numberofthreads = 10
+
+class workerthread(threading.Thread):
+	def __init__(self, input, id):
 		threading.Thread.__init__(self)
-		self.queue = queue
-		self.id = str(id)
+		self.input = input
+		self.id = id
 	
 	def run(self):
-		print "In worker thread"
 		while True:
-			counter = str(self.queue.get())
-			print "worker " + self.id + " " + counter
-			self.queue.task_done()
+			sitestring = self.input.get()
+			print sitestring
+			print "Thread %d processting %s"%(self.id,sitestring)
+			q = self.id
+			w = sitestring
+			ftp = ftplib.FTP(sitestring)
+			ftp.login()
+			ftp.retrlines("LIST", lambda q, w=self.id, e=sitestring : callback(q, w, e))
+			ftp.close()
+			print "thread %d done"%self.id
+			self.input.task_done()
+		
 
-myqueue = Queue.Queue()
+def callback(data, number, sitestring):
+#	print "number = %d"%number
+#	print "data = " + str(data)
+	txt = file(sitestring, "a")	
+	txt.write(data + "\n")
+	txt.close()
+#	print "thread %d wrote a line for %s"%(number, sitestring) 
+		
+ftpsites =  ("ftp.cisco.com", "ftp.blackbox.com", "ftp.dell.com", "ftp.hp.com", "ftp.microsoft.com", "ftp.supermicro.com", "ftp.slackware.com")
 
+queue = Queue.Queue()
 
-for x in range(10):
-	worker = WorkerThread(myqueue, x)
+for x in range(numberofthreads):
+	worker = workerthread(queue,x)
 	worker.setDaemon(True)
 	worker.start()
 
-for x in range(101):
-	myqueue.put(x)
+for x in ftpsites:
+	queue.put(x)
 
-myqueue.join()
-
-print "done!"
+queue.join()
+print "All done!"
